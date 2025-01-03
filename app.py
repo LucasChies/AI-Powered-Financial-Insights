c25f6a5657e313d803e457c32f876fe7bc01bc3d
Lucas Chies <lchies@gmail.com>
2025-01-03 11:42:29 -0300

    py

--- app.py
+++ app.py
@@ -498,6 +498,10 @@ def compare_stocks():
     return render_template('comparison_form.html')
 
 
+""" 
+if __name__ == '__main__':
+    app.run(debug=True) """
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 for local testing
+    serve(app, host="0.0.0.0", port=port)
